f467c626b88a5d40f44adc8bb940648e02930460
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-10-20 11:07:04 -0300

    init: initialize project

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+    """
+    Function to add two numbers and return the result
+    """
+
+    result = a + b
+    return result
+
+
+
+result_add = add(10.0, 5.0)
+print(result_add)
